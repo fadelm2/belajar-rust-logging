a9416dfcd3b27a4f968e34cc0144f93ecaf69abd
fadelm2 <fadelmuhammad128@gmail.com>
2025-12-26 10:41:48 +0700

    selesai loggin rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+    use log::{debug, error, info, trace, warn};
+
+    
+    #[test]
+    fn test_log(){
+        log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
+
+        error!("This is an error");
+        warn!("This is a warning");
+        info!("This is an info");
+        debug!("This is a debug");
+        trace!("This is a trace");
+    }
+}
+
+
+
+#[cfg(test)]
+mod tests2 {
+
+    use log::{debug, error, info, trace, warn};
+
+    #[test]
+    fn test_log(){
+        log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
+
+        error!("This is an error");
+        warn!("This is a warning");
+        info!("This is an info");
+        debug!("This is a debug");
+        trace!("This is a trace");
+    }
+
+}
